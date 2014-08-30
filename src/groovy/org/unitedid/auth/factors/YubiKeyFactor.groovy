c93b36ca46853c92ac47963d04203a67f276bcac
Stefan Wold <stefan.wold@unitedid.org>
2014-08-30 11:50:35 +0200

    Fix issue with multiple Yubikeys attached to a user account

--- src/groovy/org/unitedid/auth/factors/YubiKeyFactor.groovy
+++ src/groovy/org/unitedid/auth/factors/YubiKeyFactor.groovy
@@ -1,6 +1,7 @@
 package org.unitedid.auth.factors
 import com.yubico.client.v2.YubicoClient
 import grails.util.Holders
+import groovy.util.logging.Log4j
 import org.unitedid.auth.backend.CredentialStore
 import org.unitedid.auth.factors.impl.FactorImpl
 import org.unitedid.auth.hasher.impl.Hasher
@@ -9,6 +10,7 @@ import javax.crypto.SecretKey
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 
+@Log4j
 class YubiKeyFactor implements FactorImpl {
 
     static def config = Holders.config
@@ -66,13 +68,12 @@ class YubiKeyFactor implements FactorImpl {
 
         iterations = credentialStore.credential.iterations
 
-        def yubikeyResponse = verifyYubiKeyOtp()
-        if (yubikeyResponse.status == "OK") {
-            calculatePublicIdHash(yubikeyResponse.publicId)
+        // TODO: temporary quick fix for OTP_REPLAYED issue when a user have got multiple Yubikeys attached to their account
+        def tmpPublicId = YubicoClient.getPublicId(userCode)
+        calculatePublicIdHash(tmpPublicId)
 
-            if (publicIdHash == credentialStore.credential.publicIdHash) {
-                return true
-            }
+        if (publicIdHash == credentialStore.credential.publicIdHash && verifyYubiKeyOtp().status == "OK") {
+            return true
         }
 
         return false
@@ -95,13 +96,16 @@ class YubiKeyFactor implements FactorImpl {
             switch (response.getStatus().toString()) {
                 case "OK":
                     def publicId = YubicoClient.getPublicId(userCode)
+                    log.debug("YubiKey OTP OK for credentialId " + credentialId)
                     return [publicId: publicId, status: "OK"]
                     break
                 case "BAD_OTP":
+                    log.debug("YubiKey BAD_OTP for credentialId " + credentialId)
                     return [publicId: null, status: "BAD_OTP"]
                     break
                 case "REPLAYED_OTP":
-                    def publicId = YubicoClient.getPublicId(otp)
+                    def publicId = YubicoClient.getPublicId(userCode)
+                    log.debug("YubiKey REPLAYED_OTP for credentialId " + credentialId)
                     return [publicId: publicId, status: "REPLAYED_OTP"]
                     break
             }
